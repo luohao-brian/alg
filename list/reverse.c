f4bc5f78047cee1b83eba17bdb41b2484bd6e134
Luo Hao <luohao.brian@gmail.com>
2020-07-08 03:32:03 +0800

    双指针实现链表翻转

--- list/reverse.c
+++ list/reverse.c
@@ -5,6 +5,7 @@
 
 
 /*
+ * 递归方式：
  * https://zhuanlan.zhihu.com/p/86745433
  */
 Node* reverse_list (Node *head) {
@@ -15,12 +16,29 @@ Node* reverse_list (Node *head) {
     return last;
 }
 
+/*
+ * 双指针方式:
+ * https://zhuanlan.zhihu.com/p/75576160
+ */
+Node* reverse_list2 (Node *head) {
+    Node* pre = NULL;
+    Node* cur = head;
+
+    while (cur != NULL) {
+        Node* temp = cur->next;
+        cur->next = pre;
+        pre = cur;
+        cur = temp;
+    }
+
+    return pre;
+}
 
 int main () {
     Node* head = create_list(10);
     printf("Before list_reverse\n");
     print_list(head);
-    head = reverse_list(head);
+    head = reverse_list2(head);
     printf("After list_reverse\n");
     print_list(head);
     destroy_list(head);
